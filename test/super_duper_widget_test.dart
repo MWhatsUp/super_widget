a6c26921c3f46e97e22171b9df817076536e5a17
MWhatsUp <44034470+MWhatsUp@users.noreply.github.com>
2025-05-23 08:45:47 -0300

    Update and rename super_widget_test.dart to super_duper_widget_test.dart

--- test/super_duper_widget_test.dart
+++ test/super_duper_widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:super_widget/super_widget.dart';
+import 'package:super_duper_widget/super_duper_widget.dart';
 
 void main() {
   test('adds one to input values', () {
